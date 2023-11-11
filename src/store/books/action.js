90f95816a1d65d3a16e879876989ee7dd5434269
Ian Septiana <insaidseptiana@gmail.com>
2023-11-11 15:10:53 +0700

    refactor action book

--- src/store/books/action.js
+++ src/store/books/action.js
@@ -1,35 +1,25 @@
 import { createAction } from '../../utils/reducer';
 import { BOOKS_ACTION_TYPES } from './types';
 
+
+function  isSuccess(action ,books) {
+    return createAction(action, books);
+}
+
+function isFailed(action ,error) {
+    return createAction(action, error);
+}
 function fetchBooksStart() {
     return createAction(BOOKS_ACTION_TYPES.FETCH_BOOKS_START);
-}
-
-function fetchBooksSuccess(books) {
-    return createAction(BOOKS_ACTION_TYPES.FETCH_BOOKS_SUCCESS, books);
-}
-
-function fetchBooksFailed(error) {
-    return createAction(BOOKS_ACTION_TYPES.FETCH_BOOKS_FAILED, error);
 }
 
 function insertBooksStart({isbn, title}) {
     return createAction(BOOKS_ACTION_TYPES.INSERT_BOOKS_START, {isbn, title});
 }
 
-function insertBooksSuccess(book) {
-    return createAction(BOOKS_ACTION_TYPES.INSERT_BOOKS_SUCCESS, book);
-}
-
-function insertBooksFailed(error) {
-    return createAction(BOOKS_ACTION_TYPES.INSERT_BOOKS_FAILED, error);
-}
-
 export {
+    isSuccess,
+    isFailed,
     fetchBooksStart,
-    fetchBooksSuccess,
-    fetchBooksFailed,
     insertBooksStart,
-    insertBooksSuccess,
-    insertBooksFailed
 };
--- src/store/books/saga.js
+++ src/store/books/saga.js
@@ -1,23 +1,23 @@
 import { all, call, takeLatest, put } from 'typed-redux-saga';
 import { BOOKS_ACTION_TYPES } from './types';
-import { fetchBooksFailed, fetchBooksSuccess, insertBooksFailed, insertBooksSuccess } from './action';
+import { isSuccess, isFailed } from './action';
 import api from '../../data/api';
 
 export function* fetchBooks() {
     try {
         const books = yield* call(api.getAllBooks);
-        yield* put(fetchBooksSuccess(books));
+        yield* put(isSuccess(BOOKS_ACTION_TYPES.FETCH_BOOKS_SUCCESS, books));
     } catch (error) {
-        yield* put(fetchBooksFailed(error));
+        yield* put(isFailed(BOOKS_ACTION_TYPES.FETCH_BOOKS_FAILED, error));
     }
 }
 
 export function* insertBook({payload: {isbn, title}}) {
     try {
         const book = yield* call(api.createBook, {isbn, title});
-        yield* put(insertBooksSuccess(book));
+        yield* put(isSuccess(BOOKS_ACTION_TYPES.INSERT_BOOKS_SUCCESS, book));
     } catch (error) {
-        yield* put(insertBooksFailed(error));
+        yield* put(isFailed(BOOKS_ACTION_TYPES.INSERT_BOOKS_FAILED, error));
     }
 }
 
