a31203152940106c800d08b4d8b269369c196491
Yasir Amri <yasiramrr@gmail.com>
2023-11-14 16:46:26 +0700

    membuat register

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,3 +1,4 @@
+import useInput from '../hooks/useInput';
 import { useSelector } from 'react-redux';
 import { selectAuth } from '../store/auth/selector';
 import { Navigate } from 'react-router-dom';
@@ -5,6 +6,13 @@ import { Navigate } from 'react-router-dom';
 const Register = () => {
     // jangan diubah 
     const { currentUser } = useSelector(selectAuth);
+    const [name, setName] = useInput('');
+    const [email, setEmail] = useInput('');
+    const [password, setPassword] = useInput();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    };
     
     if (currentUser) {
         return <Navigate to={'/'} replace/>;
@@ -12,7 +20,15 @@ const Register = () => {
     // jangan diubah 
 
     return (
-        <div>Register</div>
+        <div>
+            Register
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="name" value={name} onChange={setName} />
+                <input type="email" placeholder="email" value={email} onChange={setEmail} />
+                <input type="password" placeholder="password" value={password} onChange={setPassword} />
+                <button type="submit">Register</button>
+            </form>
+        </div>
     );
 };
 
