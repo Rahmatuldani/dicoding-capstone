a6bb394ea4c99738a4ed59ce5b5d2bd0db0ddb7c
Ahmad Mujahid Akbar <mujahidakbar001@gmail.com>
2023-11-16 10:15:29 +0800

    Fixed display of hero and book list elements

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,12 +1,13 @@
-import { Container, Row, Col, Card } from 'react-bootstrap';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { selectBooks } from '../../store/books/selector';
 import { fetchBooksStart } from '../../store/books/action';
 
 const Home = () => {
     const dispatch = useDispatch();
     const { books } = useSelector(selectBooks);
+    const [showAll, setShowAll] = useState(false);
 
     useEffect(() => {
         dispatch(fetchBooksStart());
@@ -14,11 +15,10 @@ const Home = () => {
 
     return (
         <>
-            {/* Hero Section */}
             <section className="bg-primary text-light p-5">
                 <Container>
                     <Row>
-                        <Col>
+                        <Col className="text-center">
                             <h1>Selamat Datang di Librify</h1>
                             <p>Pinjam dan baca berbagai buku menarik di sini.</p>
                         </Col>
@@ -26,23 +26,31 @@ const Home = () => {
                 </Container>
             </section>
 
-            {/* Book List Section */}
             <Container className="my-5">
                 <h2 className="text-center mb-4">Daftar Buku Terbaru</h2>
         
                 <Row>
-                    {books.map((book) => (
-                        <Col md={3} className="mb-3" key={book.id}>
+                    {books.slice(0, showAll ? books.length : 6).map((book) => (
+                        <Col md={2} className="mb-2" key={book.id}>
                             <Card>
-                                <Card.Img src={book.image} />
+                                <Card.Img
+                                    variant="top"
+                                    src={book.image}
+                                />
                                 <Card.Body>
                                     <Card.Title>{book.title}</Card.Title>
-                                    <Card.Text>{book.description}</Card.Text>
+                                    <Card.Text>{book.author}</Card.Text>
                                 </Card.Body>
                             </Card>
                         </Col>
                     ))}
                 </Row>
+
+                <div className='d-flex justify-content-center'>
+                    <Button onClick={() => setShowAll(!showAll)}>
+                        {showAll? 'Lihat Lebih Sedikit' : 'Lihat Semua'}
+                    </Button>
+                </div>
         
             </Container>
         </>
