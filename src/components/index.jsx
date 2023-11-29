debe8fea1a96923f380d02f4b82079df9e0f8594
Rahmatul Ramadhani <danirahmatul@gmail.com>
2023-11-29 15:54:38 +0700

    unfinished

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,9 +1,11 @@
 import AvatarComponent from './avatar';
 import Loading from './loading';
 import Navbar from './navbar';
+import Sidebar from './sidebar';
 
 export {
     Loading,
     Navbar,
+    Sidebar,
     AvatarComponent,
 };
