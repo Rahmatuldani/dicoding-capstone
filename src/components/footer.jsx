0a9f85acba004e7f98b49ab8b1d3e6df6f494102
Ahmad Mujahid Akbar <mujahidakbar001@gmail.com>
2023-12-07 12:28:57 +0800

    Fixed footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,11 +1,12 @@
 import { Box, Typography } from '@mui/material';
 import { Container, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
         <Box component="footer" sx={{ p: 2, backgroundColor: '#407BFF', color: 'white' }}>
             <Container className='py-5'>
-                <Row>
+                <Row style={{ justifyContent: 'space-around' }}>
                     <Col md={3}>
                         <Typography variant='h6'>LIBRIFY</Typography>
                         {/* Logo */}
@@ -13,21 +14,19 @@ const Footer = () => {
 
                     <Col md={3}>
                         <Typography variant='h6'>Tentang Kami</Typography>
-                        <Typography variant='body1'>Pengembang</Typography>
-                    </Col>
-
-                    <Col md={3}>
-                        <Typography variant='h6'>Kontak</Typography>
                         <Typography variant='body1'>
-                            Alamat <br/>
-                            No. Telepon <br/> 
-                            Email
+                            <Link to='https://www.linkedin.com/in/rahmatulramadhani' target='_blank' rel='noopener noreferrer' style={{textDecoration: 'none', color: 'white'}}>Rahmatul Ramadhani</Link> <br/>
+                            <Link to='https://www.linkedin.com/in/ianseptiana' target='_blank' rel='noopener noreferrer' style={{textDecoration: 'none', color: 'white'}}>Ian Septiana</Link> <br/>
+                            <Link to="https://www.linkedin.com/in/amujahidakbar" target='_blank' rel='noopener noreferrer' style={{textDecoration: 'none', color: 'white'}}>Ahmad Mujahid Akbar</Link> <br/>
+                            <Link target='_blank' rel='noopener noreferrer' style={{textDecoration: 'none', color: 'white'}}>Yasir Amri</Link> <br/>
                         </Typography>
                     </Col>
 
                     <Col md={3}>
-                        <Typography variant='h6'>Sosial Media</Typography>
-                        {/* Ikon sosial media */}
+                        <Typography variant='h6'>Repository</Typography>
+                        <Typography variant='body1'>
+                            <Link to='https://github.com/Rahmatuldani/dicoding-capstone' target='_blank' rel='noopener noreferrer' style={{textDecoration: 'none', color: 'white'}}>GitHub</Link>
+                        </Typography>
                     </Col>
                 </Row>
             </Container>
