7a6b14912e359f9622ee5cd237546542e5c33b81
Ahmad Mujahid Akbar <mujahidakbar001@gmail.com>
2023-12-16 17:34:30 +0800

    Removed 'Keranjang' feature and replaced it with 'Riwayat Peminjaman'

--- src/pages/borrow/formBorrow.jsx
+++ src/pages/borrow/formBorrow.jsx
@@ -1,8 +1,7 @@
 import { useEffect, useState } from 'react';
-import { Button, Container, Table } from 'react-bootstrap';
-import Form from 'react-bootstrap/Form';
+import { Container, Table } from 'react-bootstrap';
 
-const FormBorrow = (params) => {
+const FormBorrow = () => {
     const [borrowedBooks, setBorrowedBooks] = useState([]);
     const [bookStates, setBookStates] = useState([]);
 
@@ -13,76 +12,34 @@ const FormBorrow = (params) => {
 
     useEffect(() => {
         if (borrowedBooks.length > 0) {
-            setBookStates(borrowedBooks.map(() => ({ isBookChecked: false, bookQuantity: 0 })));
+            setBookStates(borrowedBooks.map(() => ({ isBookChecked: false, bookQuantity: 1 })));
         }
     }, [borrowedBooks]);
 
-    const handleBookCheckChange = (index) => {
-        setBookStates((prevStates) => {
-            const newStates = [...prevStates];
-            newStates[index] = {
-                ...newStates[index],
-                isBookChecked: !newStates[index].isBookChecked,
-            };
-            return newStates;
-        });
-    };
-
-    const handleBookQuantityChange = (index, value) => {
-        setBookStates((prevStates) => {
-            const newStates = [...prevStates];
-            newStates[index].bookQuantity = value;
-            return newStates;
-        });
-    };
-
     return (
         <Container>
-            <h2 className='mb-3'>Keranjang</h2>
-            <Form onSubmit={params.onInputSubmit}>
-                <Table striped bordered>
-                    <thead>
-                        <tr>
-                            <th>Pilih</th>
-                            <th>Judul Buku</th>
-                            <th>Jumlah Buku</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {bookStates.length > 0 &&
+            <h2 className='mb-3'>Riwayat Peminjaman</h2>
+            <Table striped bordered>
+                <thead>
+                    <tr>
+                        <th>Judul Buku</th>
+                        <th>Jumlah Buku</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {bookStates.length > 0 &&
                             borrowedBooks.map((borrowedBook, index) => (
                                 <tr key={index}>
                                     <td>
-                                        <Form.Check
-                                            type='checkbox'
-                                            checked={bookStates[index].isBookChecked}
-                                            onChange={() => handleBookCheckChange(index)}
-                                        />
+                                        {borrowedBook.title}
                                     </td>
                                     <td>
-                                        <Form.Control
-                                            required
-                                            type='text'
-                                            placeholder='Masukkan Judul Buku'
-                                            value={borrowedBook.title}
-                                            readOnly
-                                        />
-                                    </td>
-                                    <td>
-                                        <Form.Control
-                                            required
-                                            type='number'
-                                            placeholder='Jumlah Buku'
-                                            value={bookStates[index].bookQuantity}
-                                            onChange={(e) => handleBookQuantityChange(index, e.target.value)}
-                                        />
+                                        {bookStates[index].bookQuantity}
                                     </td>
                                 </tr>
                             ))}
-                    </tbody>
-                </Table>
-                <Button type='button' onClick={params.onInputSubmit}>Pinjam</Button>
-            </Form>
+                </tbody>
+            </Table>
         </Container>
     );
 };
--- src/pages/borrow/index.jsx
+++ src/pages/borrow/index.jsx
@@ -1,100 +1,22 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectBorrow } from '../../store/borrow/selector';
-import { addBorrowStart, fetchBorrowStart } from '../../store/borrow/action';
-import { Loading } from '../../components';
+import { useState } from 'react';
 import useInput from '../../hooks/useInput';
 import FormBorrow from './formBorrow';
-import { Container, Modal, Table, Button } from 'react-bootstrap';
 
 const Borrow = () => {
-    const { borrow, isLoading } = useSelector(selectBorrow);
-    const dispatch = useDispatch();
-    const [isBookChecked, setIsBookChecked] = useState(false);
     const [bookTitle, setBookTitle] = useInput('');
     const [bookQuantity, setBookQuantity] = useState('');
-    const [showConfirmationModal, setShowConfirmationModal] = useState(false);
-
-    function handleShowModal() {
-        setShowConfirmationModal(true);
-    }
-
-    function handleCloseModal() {
-        setShowConfirmationModal(false);
-    }
-
-    function handleSubmit() {
-        if (isBookChecked && bookTitle && bookQuantity) {
-            dispatch(addBorrowStart({
-                isBookChecked,
-                bookTitle,
-                bookQuantity,
-            }));
-        }
-        handleCloseModal();
-    }
-
-    React.useEffect(() => {
-        dispatch(fetchBorrowStart({ id: 1 }));
-    }, [dispatch]);
 
     return (
         <section className='m-4'>
             <h1 className='text-center mb-4'>Peminjaman Buku</h1>
             <section className='d-flex flex-wrap justify-content-center gap-4'>
                 <FormBorrow
-                    onInputSubmit={handleShowModal}
-                    isBookChecked={isBookChecked}
-                    setIsBookChecked={setIsBookChecked}
                     bookTitle={bookTitle}
                     setBookTitle={setBookTitle}
                     bookQuantity={bookQuantity}
                     setBookQuantity={setBookQuantity}
                 />
             </section>
-
-            <Modal show={showConfirmationModal} onHide={handleCloseModal} centered>
-                <Modal.Header closeButton>
-                    <Modal.Title>Konfirmasi Peminjaman</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    Apakah Anda yakin ingin membuat pinjaman?
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleCloseModal}>
-                        Batal
-                    </Button>
-                    <Button variant="primary" onClick={handleSubmit}>
-                        Buat Pinjaman
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-            
-            <Container>
-                <h2 className='mb-3 mt-3'>Riwayat Peminjaman</h2>
-                {isLoading ? (
-                    <Loading />
-                ) : (
-                    <section>
-                        <Table striped bordered>
-                            <thead>
-                                <tr>
-                                    <th>Judul Buku</th>
-                                    <th>Jumlah Buku</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {borrow.map((item, index) => (
-                                    <tr key={index}>
-                                        <td>{item.bookTitle}</td>
-                                        <td>{item.bookQuantity}</td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </Table>
-                    </section>
-                )}
-            </Container>
         </section>
     );
 };
